de8775db641c2f558ef217791b656b48546b0aaa
awirshj <awirshjprogramming@gmail.com>
2023-07-08 21:30:00 +0330

    heeeb

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,9 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from 'svelte-adapter-github';
 
 export default {
 
-    kit: {
-        // default options are shown
-        adapter: adapter({
-   pages: 'docs',
-      assets: 'docs',
-      fallback: null,
-      precompress: false,
-      domain: '',
-      jekyll: false
-        })
-    },
+ 
+   
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
